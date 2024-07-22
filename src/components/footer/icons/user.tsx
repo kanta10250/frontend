d4cb565f8ef64a6feccc39d6ac184e457fcc9ebf
minagishl <mvxd568.co@icloud.com>
2024-07-22 18:22:37 +0900

    Fill バージョンを追加

--- src/components/footer/icons/user.tsx
+++ src/components/footer/icons/user.tsx
@@ -1,4 +1,18 @@
-export default function User() {
+export default function User({ fill = false }: { fill?: boolean }) {
+  if (fill) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="28px"
+        viewBox="0 -960 960 960"
+        width="28px"
+        fill="#000000"
+      >
+        <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z" />
+      </svg>
+    );
+  }
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
