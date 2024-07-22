3d0d0890da7b671cfdde8d9215baf51700c53bdd
minagishl <mvxd568.co@icloud.com>
2024-07-22 17:39:09 +0900

    不要なインポートを削除

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default function Map() {
