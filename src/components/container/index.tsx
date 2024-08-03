d709674eae2c8e85daf2e31681dcabf705abf92f
minagishl <mvxd568.co@icloud.com>
2024-08-03 22:15:44 +0900

    画面の完成

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -3,5 +3,12 @@ export default function Container({
 }: {
   children: React.ReactNode;
 }): JSX.Element {
-  return <div id="container">{children}</div>;
+  return (
+    <div id="container" className="flex h-screen flex-col">
+      <div className="fixed left-0 top-0 z-[9999] hidden h-screen w-screen items-center justify-center bg-white md:flex">
+        <p>現在このデバイスはサポートされていません</p>
+      </div>
+      {children}
+    </div>
+  );
 }
