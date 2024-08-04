04ce13d3b28292ece2611f1f9a1fb3b190cb32df
minagishl <mvxd568.co@icloud.com>
2024-08-05 06:06:38 +0900

    リサイズできない様に修正

--- src/components/posts/index.tsx
+++ src/components/posts/index.tsx
@@ -96,7 +96,7 @@ export default function Posts() {
                 id="description"
                 name="description"
                 required
-                className="mt-1 block w-full rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
+                className="mt-1 block w-full resize-none rounded-md border border-gray-300 px-3 py-2 shadow-sm focus:border-blue-500 focus:outline-none focus:ring-blue-500 sm:text-sm"
               />
             </div>
             <div>
