a303f963092a442d8409c03f3cb2e9e995389a75
みなぎし <mvxd568.co@icloud.com>
2024-08-13 07:32:36 +0000

    マーカーが表示できてしまうバグを修正

--- src/components/maps/events/MarkerEvent.tsx
+++ src/components/maps/events/MarkerEvent.tsx
@@ -1,6 +1,7 @@
 import { useMapEvent } from 'react-leaflet';
 import L from 'leaflet';
 import { useMarkerContext } from '@/context/markerContext';
+import { useActionContext } from '@/context/actionContext';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
@@ -13,8 +14,11 @@ L.Icon.Default.mergeOptions({
 
 export default function MarkerEvent() {
   const { markerState, setMarkerState } = useMarkerContext();
+  const { buttonState } = useActionContext();
 
   const map = useMapEvent('click', async (location) => {
+    if (!buttonState) return;
+
     if (markerState) {
       map.removeLayer(markerState);
     }
