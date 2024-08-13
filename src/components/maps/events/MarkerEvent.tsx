3557c94636ff5fdc2e7286c7e698bb10128773d9
みなぎし <mvxd568.co@icloud.com>
2024-08-13 07:24:50 +0000

    投稿時にAPIを実行するように変更

--- src/components/maps/events/MarkerEvent.tsx
+++ src/components/maps/events/MarkerEvent.tsx
@@ -5,15 +5,6 @@ import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
-interface Address {
-  city?: string;
-  suburb?: string;
-}
-
-interface Data {
-  address?: Address;
-}
-
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x.src,
@@ -21,7 +12,7 @@ L.Icon.Default.mergeOptions({
 });
 
 export default function MarkerEvent() {
-  const { markerState, setMarkerState, setNowLocation } = useMarkerContext();
+  const { markerState, setMarkerState } = useMarkerContext();
 
   const map = useMapEvent('click', async (location) => {
     if (markerState) {
@@ -31,20 +22,6 @@ export default function MarkerEvent() {
     const marker = L.marker(location.latlng).addTo(map);
 
     setMarkerState(marker);
-
-    const lat = location.latlng.lat;
-    const lng = location.latlng.lng;
-
-    // reverse geocoding
-    const response = await fetch(
-      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`,
-    );
-    const data: Data = await response.json();
-    const address = data.address;
-
-    if (address) {
-      setNowLocation(`${address.city}${address.suburb}`);
-    }
   });
 
   return null;
