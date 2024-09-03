c9341236326c0459c11ffe2c9e4d356563b32f76
minagishl <mvxd568.co@icloud.com>
2024-09-03 23:29:06 +0900

    フッターのバグ修正

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -74,7 +74,7 @@ export default function Footer() {
           item.title === 'Post' || item.title === 'Map' ? (
             <button
               key={index}
-              onClick={toggle}
+              onClick={item.title === 'Map' ? disabledButtonState : toggle}
               className="cursor-pointer rounded-full bg-white p-2 px-4"
             >
               {item.icon(buttonState ? 'true' : 'false', pathname)}
